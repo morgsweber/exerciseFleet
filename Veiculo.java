c796c0f73de0a51f8378f84fc40f5207724f108f
fernandarosa <fernanda.frosa@icloud.com>
2021-08-19 20:26:41 -0300

    getters e setters Veiculo

--- Veiculo.java
+++ Veiculo.java
@@ -14,4 +14,26 @@ public class Veiculo{
         this.valor = valor;
     }
 
+    public String getPlaca(){ return placa;}
+    public String getMarca(){ return marca;}
+    public String getModelo(){ return modelo;}
+    public int getAno(){ return ano;}
+    public double getValor(){ return valor;
+
+    public void setPlaca(String nova_placa){
+        this.placa = nova_placa;
+    }
+    public void setMarca(String nova_marca){
+        this.marca = nova_marca;
+    }
+    public void setModelo(String novo_modelo){
+        this.modelo = novo_modelo;
+    }
+    public void setAno(int novo_ano){
+        this.ano = novo_ano;
+    }
+    public void setValor(double novo_valor){
+        this.valor = novo_valor;
+    }
+
 }
